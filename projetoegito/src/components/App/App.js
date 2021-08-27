95b8d1ca58493557438ae48e8f300bb39c938dbe
SamaraSilva18 <samarasilvacandy@gmail.com>
2021-08-27 01:12:39 +0000

    Menu Horizontal

--- projetoegito/src/components/App/App.js
+++ projetoegito/src/components/App/App.js
@@ -1,27 +1,12 @@
 import './App.css';
 import React from 'react';
-
-import { Route, Switch } from "react-router-dom";
+import Header from '../Header/Header.jsx';
 
 function App() {
   return (
     <div>
 
         <Header />
-
-      <div className="titulo">
-        <Titulo />
-
-      </div>
-
-      <main>
-        <Switch>
-          <Route exact path="/" render = {(props) => <Inicio/>}> </Route>
-        </Switch>
-
-      </main>
-
-        <Footer />
 
     </div>
 
