bcc677aa9a998dc48be540e443e15952ef887e0a
SamaraSilva18 <samarasilvacandy@gmail.com>
2021-08-31 21:19:43 +0000

    Pagina Mitologia

--- projetoegito/src/components/Mitologia/Mitologia.jsx
+++ projetoegito/src/components/Mitologia/Mitologia.jsx
@@ -8,6 +8,7 @@ export default function Mitologia() {
             <section id="mitobody">
             <h2>As Contendas de Horus e Seth</h2>
             <img src={horus} alt="Horus vs Seth" className="imamito"/>
+            <h2>Sobre o Mito:</h2>
                 Bem-vindo a Segredos Milenares! Um canto da internet dedicado a tudo sobre o Egito Antigo.
             </section>
         </div>
