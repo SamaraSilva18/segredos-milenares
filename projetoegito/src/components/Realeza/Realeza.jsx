4ff4ef5fc74381faa64904ddfba6fda90510c4bb
Samara <samarasilvacandy@gmail.com>
2021-09-11 14:38:46 -0300

    Progresso no Cadastro

--- projetoegito/src/components/Realeza/Realeza.jsx
+++ projetoegito/src/components/Realeza/Realeza.jsx
@@ -9,35 +9,55 @@ export default function Realeza() {
           <div class="col">
           <h2>Nobres e Reis do Egito Antigo</h2>
           </div>
-        </div>
       <section class="container">
-      <div class="row">
-          <div class="box1">
-            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/27/CairoEgMuseumTaaMaskMostlyPhotographed.jpg/250px-CairoEgMuseumTaaMaskMostlyPhotographed.jpg" alt="Farao Tutankamon" class="img-thumbnail"></img>
+        <div class="row">
+        <div class="box1">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/27/CairoEgMuseumTaaMaskMostlyPhotographed.jpg/250px-CairoEgMuseumTaaMaskMostlyPhotographed.jpg" alt="Faraó Tutankamon" class="img-thumbnail"></img>
             <div>  
               Tutankamon subiu ao trono com 8 anos de idade. Ele reinou ate seus 19 anos e priorizou boas relaçoes com as naçoes vizinhas durante seu reinado. Ele era um grande fa de patos.
             </div>
             <a href="https://pt.wikipedia.org/wiki/Tutanc%C3%A2mon">Saiba Mais +</a>
           </div>
           <div class="box2">
-            <img src="https://unsplash.com/photos/nQCoEj8ogwE" alt="Farao Hatshepsut" class="img-thumbnail"></img>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/StatueOfHoremhebAndTheGodHorus-DetailOfHoremheb01_KunsthistorischesMuseum_Nov13-10.jpg/200px-StatueOfHoremhebAndTheGodHorus-DetailOfHoremheb01_KunsthistorischesMuseum_Nov13-10.jpg" alt="Faraó Horemheb" class="img-thumbnail"></img>
+            <div>  
+              Horemheb nasceu fora da nobreza, como filho de camponeses. Sua proeza como soldado e eventualmente general no exercito egipicio permitiu sua entrada no palacio durante o reinado de Tutankamon. Ele foi nomeado herdeiro interino e subiu ao trono apos a morte de Tut.
+            </div>
+            <a href="https://pt.wikipedia.org/wiki/Tutanc%C3%A2mon">Saiba Mais +</a>
           </div>
           <div class="box3">
-            <img src="https://unsplash.com/photos/_r9XDE_KFuMr" alt="" class="img-thumbnail"></img>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/Nefertiti_30-01-2006.jpg/220px-Nefertiti_30-01-2006.jpg" alt="Rainha Nefertiti" class="img-thumbnail"></img>
+            <div>  
+            Nefertiti teve muitos títulos, incluindo Grande de Louvores, Senhora das Graças, Senhora das Duas Terras, Senhora de Todas as Mulheres e Senhora do Alto e do Baixo Egito.
+            </div>
+            <a href="https://pt.wikipedia.org/wiki/Tutanc%C3%A2mon">Saiba Mais +</a>
           </div>
-      </div>
-      <div class="row">
+        </div>
+        <div class="row">
           <div class="box4">
             <img src="https://unsplash.com/photos/xBeid9r1paU" alt="" class="img-thumbnail"></img>
+            <div>  
+              Tutankamon subiu ao trono com 8 anos de idade. Ele reinou ate seus 19 anos e priorizou boas relaçoes com as naçoes vizinhas durante seu reinado. Ele era um grande fa de patos.
+            </div>
+            <a href="https://pt.wikipedia.org/wiki/Tutanc%C3%A2mon">Saiba Mais +</a>
           </div>
           <div class="box5">
             <img src="https://unsplash.com/photos/oCkZ6WicY8Q" alt="Farao Kufu" class="img-thumbnail"></img>
+            <div>  
+              Tutankamon subiu ao trono com 8 anos de idade. Ele reinou ate seus 19 anos e priorizou boas relaçoes com as naçoes vizinhas durante seu reinado. Ele era um grande fa de patos.
+            </div>
+            <a href="https://pt.wikipedia.org/wiki/Tutanc%C3%A2mon">Saiba Mais +</a>
           </div>
           <div class="box6">
             <img src="https://thumbs.dreamstime.com/b/est%C3%A1tua-de-nefertiti-bust-egito-antigo-rainha-eg%C3%ADpcia-exposi%C3%A7%C3%A3o-no-peter-e-paul-fortress-134138884.jpg" alt="Rainha Nefertiti" class="img-thumbnail"></img>
+            <div>  
+              Tutankamon subiu ao trono com 8 anos de idade. Ele reinou ate seus 19 anos e priorizou boas relaçoes com as naçoes vizinhas durante seu reinado. Ele era um grande fa de patos.
+            </div>
+            <a href="https://pt.wikipedia.org/wiki/Tutanc%C3%A2mon">Saiba Mais +</a>
           </div>
         </div>
-      </section>
+        </section>
+      </div>
       </div>
     </div>
   );
